a8b6912cb3295a841dfa6c85ab466c209b896aed
Vladislav Platonov <v.platonov@phoenixcas.com>
2019-06-18 22:15:48 +0300

    Make moving character working on Windows

--- Roguelike/MapToConsole.py
+++ Roguelike/MapToConsole.py
@@ -1,11 +1,10 @@
 from asciimatics.screen import Screen
-import curses
+import asciimatics.event as event
 from MapGenerator import *
 from MapLoader import *
 
 
-
-def func(map = None):
+def func(map=None):
 
     def demo(screen):
         j = 0
@@ -21,41 +20,39 @@ def func(map = None):
                     screen.print_at('#', i, j)
         screen.print_at('@', x, y)
         screen.refresh()
+
         try:
             while True:
-                stdscr = curses.initscr()
-                curses.noecho()
-                curses.cbreak()
-                stdscr.keypad(1)
-                key = stdscr.getch()
-                if key == 259 and y > 0 and map[x][y-1] != b'#':
-                    screen.print_at('.', x, y)
-                    y -= 1
-                    screen.print_at('@', x, y)
-                    screen.refresh()
-                if key == 258 and y < len(map[0]) - 1 and map[x][y+1] != b'#':
-                    screen.print_at('.', x, y)
-                    y += 1
-                    screen.print_at('@', x, y)
-                    screen.refresh()
-                if key == 261 and x < len(map) - 1 and map[x+1][y] != b'#':
-                    screen.print_at('.', x, y)
-                    x += 1
-                    screen.print_at('@', x, y)
-                    screen.refresh()
-                if key == 260 and x > 0 and map[x-1][y] != b'#':
-                    screen.print_at('.', x, y)
-                    x -= 1
-                    screen.print_at('@', x, y)
-                    screen.refresh()
+                key = screen.get_event()
+                if isinstance(key, event.KeyboardEvent):
+                    if key.key_code == Screen.KEY_UP and y > 0 and map[x][y - 1] != b'#':
+                        screen.print_at('.', x, y)
+                        y -= 1
+                        screen.print_at('@', x, y)
+                        screen.refresh()
+                    if key.key_code == Screen.KEY_DOWN and y < len(map[0]) - 1 and map[x][y+1] != b'#':
+                        screen.print_at('.', x, y)
+                        y += 1
+                        screen.print_at('@', x, y)
+                        screen.refresh()
+                    if key.key_code == Screen.KEY_RIGHT and x < len(map) - 1 and map[x + 1][y] != b'#':
+                        screen.print_at('.', x, y)
+                        x += 1
+                        screen.print_at('@', x, y)
+                        screen.refresh()
+                    if key.key_code == Screen.KEY_LEFT and x > 0 and map[x - 1][y] != b'#':
+                        screen.print_at('.', x, y)
+                        x -= 1
+                        screen.print_at('@', x, y)
+                        screen.refresh()
         except KeyboardInterrupt:
             exit(0)
 
     return demo
 
 
-
 print("Generate map - 1, load map - 2")
+
 s = input()
 if s == "1":
     print("Enter size of map")
@@ -67,5 +64,3 @@ elif s == "2":
     path = input()
     map = MapLoader(path)
     Screen.wrapper(func(map.generate_map()))
-
-
